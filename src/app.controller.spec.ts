555bcbbc9d422079be19085c41ab9e885b6a5f0d
Julien Catania <julien.catania@gmail.com>
2021-03-19 09:49:12 +0100

    TU ok

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,30 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('return 2 cats', () => {
+      expect(appController.get().length).toBe(2);
+    });
+
+    it('return Papou cat', () => {
+      expect(appController.getById('1').name).toBe('Papou');
+    });
+
+    it('add a new cat', () => {
+      expect(
+        appController.add({
+          name: 'Papou3',
+          age: 3,
+          breed: 'Marseillais',
+          id: '3',
+        }).name,
+      ).toBe('Papou3');
+
+      expect(appController.delete('3', null).name).toBe('Papou3');
+    });
+
+    it('delete Papou3 cat', () => {
+      expect(appController.delete('2', null).name).toBe('Papou2');
+      expect(appController.delete('3', null)).toBeUndefined();
     });
   });
 });
